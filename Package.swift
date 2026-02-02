84eb31ea2a3633c6c69b12ae5973ad69a4bd58a7
Jiri Banas <jiri@cberryit.com>
2026-02-02 16:27:28 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "IRKit",
-            url: "https://github.com/irdb-holdings/irkit-releases-ksl/releases/download/26.1.0/IRKit.xcframework.zip",
-            checksum: "63e0d798f69d2f22ca1edb42a0647178f00ca5a66d35339171310c289c4b991c"
+            url: "https://github.com/irdb-holdings/irkit-releases-ksl/releases/download/26.3.0/IRKit.xcframework.zip",
+            checksum: "e753f4642823700e4b1cca1ae875cc904e74299ff1f377bfc0ce4addb8e07bf2"
         )
     ]
 )
