9bd923989b8e7a7ff7d4f8d83ec2b3bcdfa57457
Jiri <jiri@cberryit.com>
2026-02-10 16:50:23 -0500

    Release 26.6.0

--- Package.swift
+++ Package.swift
@@ -1,17 +1,27 @@
-// swift-tools-version:5.9
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "IRKit",
-    platforms: [.iOS(.v17)],
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
-        .library(name: "IRKit", targets: ["IRKit"])
+        .library(
+            name: "IRKit",
+            targets: ["IRKitCore", "OnnxRuntime"]
+        )
     ],
     targets: [
         .binaryTarget(
-            name: "IRKit",
-              url: "https://github.com/irdb-holdings/irkit-releases-ksl/releases/download/26.4.1/IRKit.xcframework.zip",
-              checksum: "7ee2e2ddf130d5fd5665da5da33ff3724d37b000f25520c0527103296037a966"
+            name: "IRKitCore",
+            url: "https://github.com/irdb-holdings/irkit-releases-ksl/releases/download/26.6.0/IRKit.xcframework.zip",
+            checksum: "40a257e16b0f2964b05dbc43bbe85cd20f670057a7bbf9c70508e0d71e010196"
+        ),
+        .binaryTarget(
+            name: "OnnxRuntime",
+            url: "https://github.com/irdb-holdings/irkit-releases-ksl/releases/download/26.6.0/onnxruntime.xcframework.zip",
+            checksum: "4e7a24692e7fb315229ff0446d4c64dbfa69f33f4cd8612473643f79cf7913d2"
         )
     ]
-) 
+)
