ef1efeedf2726f87e12b7dc841e2f2a903791809
Jiri <jiri@cberryit.com>
2026-04-06 16:46:20 -0400

    Release 26.11 to KSL

--- Package.swift
+++ Package.swift
@@ -9,19 +9,14 @@ let package = Package(
     products: [
         .library(
             name: "IRKit",
-            targets: ["IRKitCore", "OnnxRuntime"]
+            targets: ["IRKitCore"]
         )
     ],
     targets: [
         .binaryTarget(
             name: "IRKitCore",
-            url: "https://github.com/irdb-holdings/irkit-releases-ksl/releases/download/26.8.0/IRKit.xcframework.zip",
-            checksum: "282c63fab9867a540a8277ca8be3276429c5f9a68c76e919b95516c90b94b491"
-        ),
-        .binaryTarget(
-            name: "OnnxRuntime",
-            url: "https://github.com/irdb-holdings/irkit-releases-ksl/releases/download/26.8.0/onnxruntime.xcframework.zip",
-            checksum: "d0cbcb19e1a1d6f051bf5ad7db6da0d8b8e71b6def3760d0b1bbb76ccb08d4dc"
+            url: "https://github.com/irdb-holdings/irkit-releases-ksl/releases/download/26.11.0/IRKit.xcframework.zip",
+            checksum: "86b4a79876d73ccc8e7ba809b0339073cbcab48d47341821d561e487e2a21884"
         )
     ]
 )
